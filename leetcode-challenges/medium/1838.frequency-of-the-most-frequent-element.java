253a615e406b885314afc499d979231480bb68aa
Drona <drona8978@gmail.com>
2026-08-04 21:28:59 +0530

    Add solution for frequency of the most frequent element problem

---
+++ leetcode-challenges/medium/1838.frequency-of-the-most-frequent-element.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1838 lang=java
+ *
+ * [1838] Frequency of the Most Frequent Element
+ */
+
+// @lc code=start
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        int left=0;
+        long totalSum=0;
+        //int maxFreq=0;
+        for(int right=0;right<nums.length;right++){
+            totalSum+=nums[right];
+
+            if((long)nums[right] * (right-left+1) - totalSum > k){
+                totalSum-=nums[left];
+                left++;
+            }
+            // maxFreq=Math.max(maxFreq,right-left+1);
+        }
+        //return maxFreq;
+        return nums.length-left;
+    }
+}
+// @lc code=end
+
