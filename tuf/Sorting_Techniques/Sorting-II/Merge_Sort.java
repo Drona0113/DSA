5b4e2ebe77d24a6c54d003e739e022efad137afd
Drona <drona8978@gmail.com>
2026-08-06 19:11:11 +0530

    Add Merge Sort implementation

---
+++ tuf/Sorting_Techniques/Sorting-II/Merge_Sort.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Merge_Sort{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size : ");
+        int n=sc.nextInt();
+        System.out.print("Enter the array : ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        mergeSort(arr,0,n-1);
+        System.out.print("Sorted array : ");
+        for (int num:arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+    }
+    public static void mergeSort(int[] arr,int low,int high){
+
+        if(low>=high) return;
+         int mid=(low+high)/2;
+         mergeSort(arr,low,mid);
+         mergeSort(arr, mid+1, high);
+         merge(arr,low,mid,high);
+
+    }
+    public static void merge(int[] arr,int low,int mid,int high){
+        List<Integer> temp = new ArrayList<>();
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(i-low);
+        }
+
+    }
+}
